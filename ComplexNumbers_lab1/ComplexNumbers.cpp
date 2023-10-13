0d975454a82c2996124ed35a37417a8d20d40a32
Альберт Загибин <guardian_96@mail.ru>
2023-10-13 20:48:50 +1000

    Реализация методов обращения к вещественной и мнимой частям

--- ComplexNumbers_lab1/ComplexNumbers.cpp
+++ ComplexNumbers_lab1/ComplexNumbers.cpp
@@ -1,2 +1,33 @@
 #include "ComplexNumbers.h"
 
+ComplexNumbers::ComplexNumbers()
+{
+	real_part = 0;
+	imaginary_part = 0;
+}
+
+ComplexNumbers::ComplexNumbers(double re, double imz)
+{
+	real_part = re;
+	imaginary_part = imz;
+}
+
+double ComplexNumbers::GetRealPart()
+{
+	return this->real_part;
+}
+
+double ComplexNumbers::SetRealPart(double number)
+{
+	return this->real_part = number;
+}
+
+double ComplexNumbers::GetImaginaryPart()
+{
+	return this->imaginary_part;
+}
+
+double ComplexNumbers::SetImaginaryPart(double number)
+{
+	return this->imaginary_part = number;
+}
