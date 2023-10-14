1a67bef1536c9cacc162b25d69b4f84c9cc1dc5d
Альберт Загибин <guardian_96@mail.ru>
2023-10-14 13:48:11 +1000

    Рефакторинг

--- ComplexNumbers_lab1/ComplexNumbers.cpp
+++ ComplexNumbers_lab1/ComplexNumbers.cpp
@@ -2,32 +2,48 @@
 
 ComplexNumbers::ComplexNumbers()
 {
-	real_part = 0;
-	imaginary_part = 0;
+	_real_part = 0;
+	_imaginary_part = 0;
 }
 
 ComplexNumbers::ComplexNumbers(double re, double imz)
 {
-	real_part = re;
-	imaginary_part = imz;
+	_real_part = re;
+	_imaginary_part = imz;
 }
 
 double ComplexNumbers::GetRealPart()
 {
-	return this->real_part;
+	return this->_real_part;
 }
 
-double ComplexNumbers::SetRealPart(double number)
+double ComplexNumbers::SetRealPart(double num_part)
 {
-	return this->real_part = number;
+	return this->_real_part = num_part;
 }
 
 double ComplexNumbers::GetImaginaryPart()
 {
-	return this->imaginary_part;
+	return this->_imaginary_part;
 }
 
-double ComplexNumbers::SetImaginaryPart(double number)
+double ComplexNumbers::SetImaginaryPart(double num_part)
 {
-	return this->imaginary_part = number;
+	return this->_imaginary_part = num_part;
 }
+
+ComplexNumbers ComplexNumbers::operator+(const ComplexNumbers& other)
+{
+	return ComplexNumbers(
+		this->_real_part + other._real_part,
+		this->_imaginary_part + other._imaginary_part
+	);
+};
+
+ComplexNumbers ComplexNumbers::operator+(const ComplexNumbers& other)
+{
+	return ComplexNumbers(
+		this->_real_part - other._real_part,
+		this->_imaginary_part - other._imaginary_part
+	);
+};
--- ComplexNumbers_lab1/ComplexNumbers.h
+++ ComplexNumbers_lab1/ComplexNumbers.h
@@ -1,30 +1,22 @@
 class ComplexNumbers
 {
 private:
-	double real_part;
-	double imaginary_part;
+	double _real_part;
+	double _imaginary_part;
 public:
 	ComplexNumbers(double re, double imz); //constructor
 	ComplexNumbers();
 	double GetRealPart();
-	double SetRealPart(double number);
+	double SetRealPart(double num_part);
 	double GetImaginaryPart();
-	double SetImaginaryPart(double number);
-
-	ComplexNumbers operator+(const ComplexNumbers &other)
-	{
-		return ComplexNumbers(
-			this->real_part + other.real_part,
-			this->imaginary_part + other.imaginary_part
-		);
-	};
-
-	ComplexNumbers operator-(const ComplexNumbers& other)
-	{
-		return ComplexNumbers(
-			this->real_part - other.real_part,
-			this->imaginary_part -	 other.imaginary_part
-		);
-	};
+	double SetImaginaryPart(double num_part);
+	static ComplexNumbers Add(ComplexNumbers& left, ComplexNumbers& rigjt);
+	static ComplexNumbers Sub(ComplexNumbers& left, ComplexNumbers& right);
+	static ComplexNumbers Mult(ComplexNumbers& left, ComplexNumbers& right);
+	static ComplexNumbers Div(ComplexNumbers& left, ComplexNumbers& right);
+	static ComplexNumbers Pow(ComplexNumbers& num, float& pow);
+	double Abs();
+	ComplexNumbers operator+(const ComplexNumbers& other);
+	ComplexNumbers operator-(const ComplexNumbers& other);
 };
 
