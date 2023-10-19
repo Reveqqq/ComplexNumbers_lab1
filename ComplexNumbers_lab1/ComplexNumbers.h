4197a5f4eb6dca3e78ab0eb75dfbd1f58fb190b6
Альберт Загибин <guardian_96@mail.ru>
2023-10-19 23:49:18 +1000

    Realization of polar system

--- ComplexNumbers_lab1/ComplexNumbers.h
+++ ComplexNumbers_lab1/ComplexNumbers.h
@@ -1,22 +1,33 @@
+#include <iostream>
+
+namespace complex {
+
 class ComplexNumbers
 {
 private:
-	double _real_part;
-	double _imaginary_part;
+		double _real;
+		double _imz;
+		double _angel;
+		double GetAngel() const;
 public:
-	ComplexNumbers(double re, double imz); //constructor
+		ComplexNumbers(double re, double imz);
 	ComplexNumbers();
+
 	double GetRealPart();
-	double SetRealPart(double num_part);
+		double SetRealPart(double& num_part);
 	double GetImaginaryPart();
-	double SetImaginaryPart(double num_part);
+		double SetImaginaryPart(double& num_part);
+
 	static ComplexNumbers Add(ComplexNumbers& left, ComplexNumbers& rigjt);
 	static ComplexNumbers Sub(ComplexNumbers& left, ComplexNumbers& right);
 	static ComplexNumbers Mult(ComplexNumbers& left, ComplexNumbers& right);
 	static ComplexNumbers Div(ComplexNumbers& left, ComplexNumbers& right);
-	static ComplexNumbers Pow(ComplexNumbers& num, float& pow);
-	double Abs();
+		static ComplexNumbers Pow(const ComplexNumbers& num, double& n);
+		double Abs() const;
+
 	ComplexNumbers operator+(const ComplexNumbers& other);
 	ComplexNumbers operator-(const ComplexNumbers& other);
+		ComplexNumbers operator*(const ComplexNumbers& other);
+		ComplexNumbers operator/(const ComplexNumbers& other);
 };
-
+}
